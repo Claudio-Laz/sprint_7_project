face6f4d72da7e8b5c1b7582aff805dbde6ede95
Claudio-Laz <claudio.lazcano.m@gmail.com>
2024-11-26 23:17:50 -0600

    project in Python

--- vehicles_us.py
+++ vehicles_us.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 
-st.write('Esta aplicación aún no es funcional. En construcción.')
+st.write('Esta aplicación aún no es funcional. En construcción. Jeje ¡Hola Clau, te ves chido!')
 
 print("Hi Claudio.")
