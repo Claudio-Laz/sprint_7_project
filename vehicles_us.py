a350baf72f927b5d1d21c160ceee71c235bf735e
Claudio-Laz <claudio.lazcano.m@gmail.com>
2024-11-27 20:54:34 -0600

    project in Python

--- vehicles_us.py
+++ vehicles_us.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.header('Lanzar una moneda')
+st.header('Se ha modificado el título.............. Holaaaaa!!')
 
 st.write('Esta aplicación aún no es funcional. En construcción. Jeje ¡Hola Clau, te ves chido!')
 
