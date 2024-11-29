fec51409731cbab55fa3befa59024eb79a21f383
Claudio-Laz <claudio.lazcano.m@gmail.com>
2024-11-29 00:22:23 -0600

    my project

--- vehicles_us.py
+++ vehicles_us.py
@@ -1,7 +1,31 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.header('Se ha modificado el título.............. Holaaaaa!!')
+df_vehicles = pd.read_csv("notebooks/vehicles_us.csv")
 
-st.write('Esta aplicación aún no es funcional. En construcción. Jeje ¡Hola Clau, te ves chido!')
+st.header("Análisis de los Anúncios de Venta para Vehículos Usados en los Estados Unidos")
+st.write("")
+st.write("A continuación, encontrará una lista con diversas secciones que al seleccionar una o varias, le mostrará el análisis con gráficos y observaciones relevantes.")
+st.divider()
 
-print("Hi Claudio.")
+vehicles_price_checkbox = st.checkbox('Precios de Vehículos')
+odometer_price_checkbok = st.checkbox('Relación entre Kilometraje y Precio')
+vehicle_type_checkbox = st.checkbox('Popularidad del Tipo de Vehículo')
+days_condition_checkbox = st.checkbox('Duración Promedio en el Mercado por Condición del Vehículo')
+paint_color_checkbox = st.checkbox('Popularidad de Colores en los Vehículos')
+st.divider()
+
+if vehicles_price_checkbox:
+    st.write(
+        "Hola")
+    fig_price_distribution = px.histogram(df_vehicles,
+                                          x="price",
+                                          nbins=50,
+                                          title="Distribución de Precios de Vehículos",
+                                          marginal="box")
+
+    fig_price_distribution.update_layout(
+        xaxis_title="Precio en USD",
+        yaxis_title="Millones de unidades")
+    st.plotly_chart(fig_price_distribution, use_container_width=True)
